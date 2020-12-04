f082859688535c14e372ff8770a3e5c12d75a811
tristan <borntowebb@gmail.com>
2020-12-04 22:25:53 +0100

    Added the contact form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { IonHeaderComponent } from './ion-header/ion-header.component';
+import { LoaderComponent } from './loader/loader.component';
 import { MaterialModule } from '../angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
     IonHeaderComponent,
+    LoaderComponent
   ],
   imports: [
     MaterialModule,
@@ -16,6 +18,7 @@ import { MaterialModule } from '../angular-material/angular-material.module';
   ],
   exports: [
     IonHeaderComponent,
+    LoaderComponent
   ]
 
 })
