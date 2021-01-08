cb0ea72c73291dc775127977e5db3ebef9fed6c4
tristan <borntowebb@gmail.com>
2021-01-08 12:36:16 +0100

    Changed the prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:7000/api'
-  // apiUrl: 'https://lacleduquai.herokuapp.com/api'
+  apiUrl: 'https://lacleduquai.herokuapp.com/api'
 };
