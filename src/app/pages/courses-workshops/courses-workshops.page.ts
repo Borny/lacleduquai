9070094cd3fb09596840efdbfd642663c4dd218a
tristan <tristan@citytaps.org>
2024-09-05 13:28:40 +0200

    Set up activities info for 2024/2025

--- src/app/pages/courses-workshops/courses-workshops.page.ts
+++ src/app/pages/courses-workshops/courses-workshops.page.ts
@@ -15,7 +15,7 @@ export class CoursesWorkshopsPage implements OnInit {
       activities: [
         {
           properties: [
-            { name: 'Atelier', value: 'Danse Contemporaine - Impro' },
+            { name: 'Atelier', value: 'Danse Contemporaine' },
             { name: 'Horaires', value: '18h30-20h30' },
             // { name: 'Niveau', value: 'Tout niveau' },
             // { name: 'Intervenant', value: 'Jérôme Chambon' },
@@ -24,42 +24,35 @@ export class CoursesWorkshopsPage implements OnInit {
         },
         {
           properties: [
-            { name: 'Atelier', value: 'Danse-Théâtre' },
+            { name: 'Atelier', value: 'Composition instantanée / GIA' },
             { name: 'Horaires', value: '20h30-22h30' },
             // { name: 'Niveau', value: 'Tout niveau' },
             // { name: 'Intervenant', value: 'Jérôme Chambon' },
-            { name: 'Lieu', value: 'Studio 71 Bordeaux' },
+            { name: 'Lieu', value: 'Studio 71 - Bordeaux' },
           ],
         },
       ],
     },
-    {
-      heading: 'Mercredi',
-      activities: [
-        {
-          properties: [
-            { name: 'Atelier', value: 'Danse Contemporaine - Pratique intensive' },
-            { name: 'Horaires', value: '19h-22h' },
-            { name: 'Lieu', value: 'Ecole Simone Veil, rue du Jardin de l\'Ars, Bordeaux' },
-          ],
-        },
-      ],
-    },
+    // {
+    //   heading: 'Mercredi',
+    //   activities: [
+    //     {
+    //       properties: [
+    //         { name: 'Atelier', value: 'Danse contemporaine avancé' },
+    //         { name: 'Horaires', value: '19h30-21h30' },
+    //         { name: 'Lieu', value: 'Studio H25 - Bordeaux' },
+    //       ],
+    //     },
+    //   ],
+    // },
     {
       heading: 'Jeudi',
       activities: [
         {
           properties: [
-            { name: 'Atelier', value: 'Danse contemporaine - Enfants' },
-            { name: 'Horaires', value: '18h-19h' },
-            { name: 'Lieu', value: 'Sore' },
-          ],
-        },
-        {
-          properties: [
-            { name: 'Atelier', value: 'Danse contemporaine - Adultes' },
-            { name: 'Horaires', value: '19h15-20h45' },
-            { name: 'Lieu', value: 'Sore' },
+            { name: 'Atelier', value: 'Danse contemporaine' },
+            { name: 'Horaires', value: '19h-21h' },
+            { name: 'Lieu', value: 'Sore - Landes' },
           ],
         },
       ],
@@ -86,19 +79,19 @@ export class CoursesWorkshopsPage implements OnInit {
     {
       heading: 'Dates',
       content: [
-        '7-8 Octobre 2023',
-        '11-12 Novembre 2023',
-        '9-10 Décembre 2023',
-        '13-14 Janvier 2024',
-        '10-11 Février 2024',
-        '9-10 Mars 2024',
-        '6-7 Avril 2024',
-        '27-28 Avril 2024',
+        '12-13 Octobre 2024',
+        '2-3 Novembre 2024',
+        '7-8 Décembre 2024',
+        '25-26 Janvier 2025',
+        '15-16 Février 2025',
+        '8-9 Mars 2025',
+        '29-30 Mars 2025',
+        '12-13 Avril 2025',
       ]
     },
     {
       heading: 'Lieu',
-      content: ['LalaLandes']
+      content: ['Sores - Landes']
     },
   ]
 
@@ -106,17 +99,11 @@ export class CoursesWorkshopsPage implements OnInit {
     {
       headers: ['Evènement', 'Date', 'Horaire', 'Lieu'],
       content: [
-        // ['Stages Eté 2023', '10-14 Juillet 2023', '10h - 17h', 'LalaLandes'],
-        ['Stage weekend 1', '14-15 Octobre 2023', '10h - 17h', 'LalaLandes'],
-        ['Stage weekend 2 - Adult', '25-26 Novembre 2023', '10h - 12h', 'Bordeaux'],
-        ['Stage weekend 2 - Enfant', '25-26 Novembre 2023', '14h - 17h', 'Bordeaux'],
-        // ['Stage mi-long 1', '23-24-25-26 Novembre 2023','10h - 17h','LalaLandes'],
-        ['Stage journée', 'Le 17 décembre 2023', '10h - 17h', 'Bordeaux'],
-        ['Stage weekend 3', 'Le 27-28 Janvier 2024','10h - 17h','Bordeaux'],
-        ['Stage semaine', 'Du 26 Février au 1er Mars 2024','10h - 17h','Bordeaux'],
-        ['Stage weekend 4', '13-14 Avril 2024','10h - 17h','LalaLandes'],
-        ['Stage weekend 5', '25-26 Mai 2024','10h - 17h','LalaLandes'],
-        ['Stage été 2024', 'du 8 au 12 Juillet 2024','10h - 17h','LalaLandes'],
+        // ['Stages Eté 2024', '10-14 Juillet 2024', '10h - 17h', 'LalaLandes'],
+        ['Le duo', '14-15 Décembre 2024', '10h - 18h', 'Bordeaux'],
+        ['Le groupe : création et improvisation', '8-9 Février 2025', '10h - 18h', 'Bordeaux'],
+        ['Occuper l\'espace', '10-11 Mai 2025', '10h - 18h', 'Sore - Landes'],
+        ['Stage intensif de création', '7-12 Juillet 2025', '10h - 18h', 'Sore - Landes'],
       ]
     },
   ]
@@ -125,11 +112,8 @@ export class CoursesWorkshopsPage implements OnInit {
     {
       headers: ['Date', 'Horaire', 'Lieu'],
       content: [
-        ['13 septembre 2023', 'Soirée', 'Bordeaux'],
-        ['25 novembre 2023', 'Soirée', 'Bordeaux'],
-        ['16 décembre 2023', 'Soirée', 'Bordeaux'],
-        ['27 janvier 2024', 'Soirée', 'Bordeaux'],
-        ['18 juin 2024', 'Soirée', 'Bordeaux'],
+        ['Samedi 14 Décembre 2024', '20h - 22h', 'Bordeaux'],
+        ['Samedi 8 Février 2025', '20h - 22h', 'Bordeaux'],
       ]
     },
   ]
@@ -138,16 +122,16 @@ export class CoursesWorkshopsPage implements OnInit {
     {
       headers: ['Cours hebdomadaire Bordeaux'],
       content: [
-        ['Quantité', '1', '2'],
-        ['Prix', '430€', '730€ (-15%)'],
+        ['Quantité', '1', '2','3'],
+        ['Prix', '430€', '770€ (-20%)', '1100€ (-20%)'],
         ['A la carte', '20€'],
       ]
     },
     {
       headers: ['Cours hebdomadaire Sore'],
       content: [
-        ['Enfants', '250€'],
-        ['Adultes', '350€'],
+        // ['Enfants', '250€'],
+        // ['Adultes', '350€'],
         ['A la carte', '15€'],
       ]
     },
@@ -164,16 +148,22 @@ export class CoursesWorkshopsPage implements OnInit {
     {
       headers: ['Stages'],
       content: [
-        ['Semaine', '370€ + logement 50€',],
-        ['Weekend', '120€',],
+        ['Semaine', '350€ + logement 50€',],
+        ['Weekend', '120€ + Logement 15€',],
         ['Journée', '75€',],
         // ['Pack (3 stages weekend ou journée dans l\'année)', '90€',],
       ]
     },
     {
-      headers: ['Offre 3 stages(hors stage d\'une semaine)'],
+      headers: ['Offre 3 stages (hors stage d\'une semaine et logement)'],
       content: [
-        ['-10% sur le total'],
+        ['-20% sur le total'],
+      ]
+    },
+    {
+      headers: ['eXpressiv\''],
+      content: [
+        ['10€'],
       ]
     },
   ]
